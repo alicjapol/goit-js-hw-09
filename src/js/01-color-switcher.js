8e4656d231a5dc5eafae89a4458ceb977857e532
alicjapol <alicjaxpoltorak@gmail.com>
2023-11-24 18:44:29 +0100

    color switcher complete

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBtn = document.querySelector("[data-start]");
+const stopBtn = document.querySelector("[data-stop]");
+let colorInterval;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener("click", () => {
+  if (!startBtn.hasAttribute("disabled")) {
+    colorInterval = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+
+    stopBtn.removeAttribute("disabled");
+    startBtn.setAttribute("disabled", true);
+  }
+});
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(colorInterval);
+  stopBtn.setAttribute("disabled", true);
+  startBtn.removeAttribute("disabled");
+});
