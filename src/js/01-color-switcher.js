929a199d882c819b52179f25a774c490c607dc72
alicjapol <alicjaxpoltorak@gmail.com>
2023-11-26 09:55:29 +0100

    files format

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,24 +1,24 @@
-const startBtn = document.querySelector("[data-start]");
-const stopBtn = document.querySelector("[data-stop]");
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 let colorInterval;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-startBtn.addEventListener("click", () => {
-  if (!startBtn.hasAttribute("disabled")) {
+startBtn.addEventListener('click', () => {
+  if (!startBtn.hasAttribute('disabled')) {
     colorInterval = setInterval(() => {
       document.body.style.backgroundColor = getRandomHexColor();
     }, 1000);
 
-    stopBtn.removeAttribute("disabled");
-    startBtn.setAttribute("disabled", true);
+    stopBtn.removeAttribute('disabled');
+    startBtn.setAttribute('disabled', true);
   }
 });
 
-stopBtn.addEventListener("click", () => {
+stopBtn.addEventListener('click', () => {
   clearInterval(colorInterval);
-  stopBtn.setAttribute("disabled", true);
-  startBtn.removeAttribute("disabled");
+  stopBtn.setAttribute('disabled', true);
+  startBtn.removeAttribute('disabled');
 });
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -20,20 +20,28 @@ const form = document.querySelector('.form');
 
 form.addEventListener('submit', handleSubmit);
 
-function handleSubmit(submit) {
-  submit.preventDefault();
-  const delay = submit.currenTarget;
-  const step = submit.currenTarget;
-  const amount = submit.currenTarget;
-  let currentDelay = Number(delay.value);
-  for (let i = 1; i <= amount.value; i += 1) {
-createPromise(i, currentDelay)
-  .then(({ position, delay }) => {
-    Notiflix.Notify.success('Fulfilled promise ${position} in ${delay}ms');
-  })
-  .catch(({ position, delay }) => {
-    Notiflix.Notify.failure('Rejected promise ${position} in ${delay}ms');
-  });
-  currentDelay += Number(step.value);
+function handleSubmit(event) {
+  event.preventDefault();
+  
+  const delayInput = form.elements['delay'];
+  const stepInput = form.elements['step'];
+  const amountInput = form.elements['amount'];
+
+  const delay = Number(delayInput.value);
+  const step = Number(stepInput.value);
+  const amount = Number(amountInput.value);
+
+  let currentDelay = delay;
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, currentDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
+
+    currentDelay += step;
   }
-  }
+}
